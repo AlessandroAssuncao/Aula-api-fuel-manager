6d83db657a99755f556ffc3c5b123468339cecf3
Alessandro Nunes Assuncao <nunes.1370451@gmail.com>
2024-09-04 11:51:28 -0300

    Criação dos modelos estabelecimento e seerviços.

---
+++ Models/Estabelecimento.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aula_api_fuel_manager.Models
+{
+    [Table("Estabelecimentos")]
+    public class Estabelecimento
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Endereco { get; set; }
+        [Required]
+        public string Telefone { get; set; }    
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        public ICollection<Servico> Servicos { get; set; }
+    }
+}
---
+++ Models/Servico.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aula_api_fuel_manager.Models
+{
+    [Table("Servicos")]
+    public class Servico
+    {
+
+
+
+        [Required]
+        public int EstablecimentoId { get; set; }
+
+        public Estabelecimento Estabelecimento { get; set; }
+    }
+}
