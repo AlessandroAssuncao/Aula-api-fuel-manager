9121f0667d1c24ebde1bd093b20fd09ba3881d8d
Alessandro Nunes Assuncao <nunes.1370451@gmail.com>
2024-09-07 17:31:01 -0300

    Codificação Create

--- Controllers/EstabelecimentosController.cs
+++ Controllers/EstabelecimentosController.cs
@@ -21,6 +21,26 @@ namespace Aula_api_fuel_manager.Controllers
             var model = await _context.Estabelecimento.ToListAsync();
 
             return Ok(model);
+
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(Estabelecimento model)
+        {
+            _context.Estabelecimento.Add(model); //Faz a inserção de Estabelecimentos no BD
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Estabelecimento.FirstOrDefaultAsync(c => c.Id == id);
+
+            if(model == null) NotFound();  // Significa que o dado não foi encontrado
+
+            return Ok(model);
+
         }
     }
 }
